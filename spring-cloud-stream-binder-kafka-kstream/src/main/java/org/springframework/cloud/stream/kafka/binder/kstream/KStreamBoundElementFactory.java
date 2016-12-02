ae8d0e66e8787537255a8e83093736e79fa1a547
Marius Bogoevici <mbogoevici@pivotal.io>
2016-12-02 16:49:41 -0500

    Updated class name
    Signed-off-by: Marius Bogoevici <mbogoevici@pivotal.io>

--- spring-cloud-stream-binder-kafka-kstream/src/main/java/org/springframework/cloud/stream/kafka/binder/kstream/KStreamBoundElementFactory.java
+++ spring-cloud-stream-binder-kafka-kstream/src/main/java/org/springframework/cloud/stream/kafka/binder/kstream/KStreamBoundElementFactory.java
@@ -3,14 +3,13 @@ package org.springframework.cloud.stream.kafka.binder.kstream;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
 
-import org.springframework.cloud.stream.binding.AbstractBoundElementFactory;
-import org.springframework.cloud.stream.binding.BoundElementFactory;
+import org.springframework.cloud.stream.binding.AbstractBindingTargetFactory;
 import org.springframework.cloud.stream.config.ChannelBindingServiceProperties;
 
 /**
  * @author Marius Bogoevici
  */
-public class KStreamBoundElementFactory extends AbstractBoundElementFactory<KStream> {
+public class KStreamBoundElementFactory extends AbstractBindingTargetFactory<KStream> {
 
 	private final KStreamBuilder kStreamBuilder;
 
